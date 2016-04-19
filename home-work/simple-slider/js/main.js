11e8c886fa85af346fe611e399b2bfc99665e3f6
sarhan1988 <967025>
2016-04-19 21:00:42 +0300

    RD simple slider

---
+++ home-work/simple-slider/js/main.js
@@ -0,0 +1,172 @@
+var timeDebugger = (function () {
+    var time = 0;
+    return {
+        setTime: function () {
+            time = Date.now();
+        },
+        getTime: function () {
+            var timeStemp = parseInt((Date.now() - time).toString(), 10);
+            if (timeStemp) {
+                console.log(timeStemp + ' milliseconds');
+            }
+        }
+    }
+}());
+
+function easySlider(cfg) {
+
+    // Constants
+    var UNIT = 'px';
+    var TO_BACK = true;
+
+    // Default params
+    cfg = cfg || {};
+    var animationDuration = cfg.animationDuration || '.5s';
+    var mouseOffsetToMove = cfg.mouseOffsetToMove || 50;
+    var animationAfter = cfg.animationAfter || 2000;
+    var moduleId = cfg.moduleId || 'easySlider';
+    var autoScroll = cfg.autoScroll || true;
+
+    // Selectors
+    var root = document.getElementById(moduleId);
+
+    // Calculations
+    var itemWidth = root.clientWidth;
+    var sliderWidth = root.children.length * itemWidth;
+    var sliderLimit = (sliderWidth - (2 * itemWidth)) * -1;
+
+    // Flags
+    var isAnimationFinished = true;
+    var isOverSlider = false;
+
+    // Other
+    var animationIndex, mouseOffsetStartPoint;
+
+
+    // Calculations Infinity slider
+    function resetSlider(toDefault) {
+        var offsetLeft = parseInt(root.style.left, 10);
+        var isResetToStart = (offsetLeft === ((sliderWidth - itemWidth) * -1));
+        var isResetToEnd = offsetLeft === 0;
+
+        if (toDefault || (isResetToStart || isResetToEnd)) {
+            root.style.transitionDuration = '';
+            root.style.width = sliderWidth + UNIT;
+
+            if (toDefault || isResetToStart) {
+                root.style.left = (itemWidth * -1) + UNIT;
+            } else if (isResetToEnd) {
+                root.style.left = sliderLimit + UNIT;
+            }
+        }
+    }
+
+    function runSlide() {
+        timeDebugger.setTime();
+        animationIndex = setTimeout(offsetSlide, animationAfter);
+    }
+
+    function init() {
+
+        resetSlider(true); // Set default data to slider
+
+        /**
+         * if autoScroll = true
+         * run slider
+         */
+        if (autoScroll) {
+            runSlide();
+        }
+
+        // Add handlers
+        addListeners();
+    }
+
+    function addListeners() {
+        root.addEventListener('transitionend', function () {
+
+            /*
+             * reset slider if it's need
+             * if it's end begin from start
+             * if it's start begin from end
+             * */
+            resetSlider();
+
+            /*
+             * isOverSlider = true
+             * mouse over slider
+             *
+             * isAnimationFinished = false
+             * animation in process
+             * */
+            timeDebugger.getTime();
+            if (isAnimationFinished && !isOverSlider) {
+                runSlide();
+            }
+
+            // define that animation finished
+            isAnimationFinished = true;
+        });
+        root.addEventListener('mousemove', function (e) {
+            e.preventDefault();
+            // Stop auto scroll
+            clearTimeout(animationIndex);
+            isOverSlider = true;
+        });
+        root.addEventListener('mousedown', function (e) {
+            e.preventDefault();
+            mouseOffsetStartPoint = e.clientX;
+        });
+        root.addEventListener('mouseout', function (e) {
+            e.preventDefault();
+            // Start auto scroll
+            isOverSlider = false;
+            runSlide();
+        });
+        root.addEventListener('mouseup', function (e) {
+            e.preventDefault();
+            var mouseOffset = mouseOffsetStartPoint - e.clientX;
+
+            // start new animation only when previous finished
+            if (isAnimationFinished) {
+
+                // Which direction to move
+                if (mouseOffset >= mouseOffsetToMove) {
+                    isAnimationFinished = false;
+                    offsetSlide();
+                } else if (mouseOffset <= -mouseOffsetToMove) {
+                    isAnimationFinished = false;
+                    offsetSlide(TO_BACK);
+                }
+            }
+        });
+    }
+
+    // Change offset
+    function offsetSlide(isToBack) {
+        var previewsOffset = parseInt(root.style.left, 10);
+
+        // Move right
+        var newOffset = (+previewsOffset) - itemWidth;
+
+        // Move left
+        if (isToBack) {
+            newOffset = (+previewsOffset) + itemWidth;
+        }
+
+        timeDebugger.setTime();
+
+        // Move
+        if (previewsOffset >= sliderLimit) {
+            root.style.transitionDuration = animationDuration;
+            root.style.left = newOffset + UNIT;
+        }
+    }
+
+    init();
+}
+
+easySlider();
+easySlider({
+    moduleId: 'easySlider2'
+});
